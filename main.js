13e56584627235f61e6895c2655b420d6ec9ec3e
ABermoTUD <a00032216@myTUDublin.ie>
2025-11-26 16:36:08 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 function validateForm() {
+    
     let name = document.getElementById("name").value;
     let email = document.getElementById("email").value;
     let message = document.getElementById("message").value;
@@ -56,3 +57,4 @@ function validateForm() {
 
     return false;
 }
+
